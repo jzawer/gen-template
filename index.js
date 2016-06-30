854ea0c1acd1fe155e8438b1f8fc9ea41502fa53
jzawer <javier271996@gmail.com>
2016-06-30 19:27:35 +0200

    Cambios

--- index.js
+++ index.js
@@ -1,44 +1,46 @@
 var nunjucks = require('nunjucks');
 var express = require('express');
-var app = require('express')();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var app = express();
+var bodyParser = require('body-parser');
+var server = require("http").createServer(app);
+var io = require("socket.io").listen(server);
+var router = express.Router();
 
 server.listen(8080, '127.0.0.1');
 
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(bodyParser.json()); // support json encoded bodies
+
 app.use(express.static('node_modules'));
+app.use(router);
+
 
 nunjucks.configure('views', {
   autoescape: true,
   express   : app
 });
 
-app.get('/', function(req, res) {
-  res.render('index.html', {
-    name: 'pepe'
-  });
-});
-
-function connect() {
-	socket.emit('sayHello');
-}
-
-io.on('connection', function (socket) {
-
-	socket.on('hi', function(data) {
-		var form_data = [];
-
-		data.forEach(function(index, element){
-
-			var name_tmp = index.name;
-			var value_tmp = index.value;
-			var tmp = [];
-			tmp[name_tmp] = value_tmp;
-
-			form_data.push(tmp);
-		});
-		console.log(form_data);
+router.get('/', function(req, res) {
+	res.render('index.html', {title: 'jose'}, function(err, html) {
+		res.send(html);
 	});
-
-
 });
+
+router.post('/generate', function(req, res) {
+	data = req.body;
+
+	res.render('templates/construction_web.html',data , function(err, html){
+		if(err) console.log(err);
+		//res.send(html);
+		// trying same but forcing status
+		res.status(200).send(html);
+	});
+});
+
+/*io.sockets.on('connection', function(socket) {
+	socket.on('generate', function(data) {
+		app.render('templates/construction_web.html', function(err, html){
+
+		});
+	});
+});*/
